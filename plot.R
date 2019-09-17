8bb8443ba3ec48bca701cbb68f663c5774de6af4
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-09-17 19:01:16 +0100

    update

--- plot.R
+++ plot.R
@@ -24,11 +24,15 @@ plot.nrc <- function(width, height) {
     column_order = column_order(H),
     row_names_side = c("left"),
     name = "NRC",
-    # col = colorRamp2(
-    #   breaks = seq(0, 2, 1),
-    #   colors = c("#A71B4B", "#FEFDBE", "#584B9F")
-    # ),
-    column_title = "Normalized relative compression (NRC)"
+    col = colorRamp2(
+      # breaks = seq(0, 2, 1),
+      # colors = c("#A71B4B", "#FEFDBE", "#584B9F")
+      seq(0, 2, 0.2),
+      hcl.colors(11, palette = "spectral")
+    ),
+    column_title = "Normalized relative compression (NRC)", 
+    # row_km = 3, row_km_repeats = 100,
+    # column_km = 3, column_km_repeats = 100
   )
 
   # pdf("nrc.pdf", width = width, height = height)
