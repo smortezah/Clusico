b17bdde65412819a076dcd613e97fc674f6ea64f
Morteza Hosseini <s_morteza_h@yahoo.com>
2019-09-18 10:30:13 +0100

    update

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import shutil
 import matplotlib.pyplot as plt
 import numpy as np
 
-find_simil_seqs = True
+find_simil_seqs = False
+make_nrc_ave = True
+
 
 if os.name == 'posix':
     sep = '/'
@@ -19,10 +21,10 @@ def execute(cmd):
 
 
 if find_simil_seqs:
-    if (os.path.exists('log')):
+    if os.path.exists('log'):
         os.remove('log')
     for file in os.listdir(dataset_path):
-        if (file.endswith('.co')):
+        if file.endswith('.co'):
             os.remove(dataset_path + file)
     if (not os.path.exists(result_path)):
         os.mkdir(result_path)
@@ -56,10 +58,29 @@ if find_simil_seqs:
                     if len(line_list) > 5:
                         out_file.write('\t' + str(line_list[5]))
             
-            if (os.path.exists(tar + '.co')):
+            if os.path.exists(tar + '.co'):
                 os.remove(tar + '.co')
         out_file.write('\n')
         
-    if (os.path.exists('log')):
+    if os.path.exists('log'):
         os.remove('log')
     print('Finished.')
+
+
+def build_nrc_ave(nrc_mat):
+    num_files = len(nrc_mat)
+    ave_mat = [[0 for x in range(num_files)] for y in range(num_files)]
+    for i in range(0, num_files):
+        for j in range(i+1, num_files):
+            ave_mat[i][j] = ave_mat[j][i] = (ave_mat[i][j] + ave_mat[j][i]) / 2
+    return ave_mat
+
+
+if make_nrc_ave:
+    nrc_file = open('result/nrc.tsv')
+    header = nrc_file.readline().split()
+    nrc_file.seek(0)
+    nrc_mat = np.genfromtxt(nrc_file, skip_header=True,
+                            usecols=range(1, len(header) + 1))
+
+    print(nrc_mat)
